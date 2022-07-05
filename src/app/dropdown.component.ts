5e74fff8e2f3bcd5dd94e168414bec8f1dcacaf6
Rohan Bhatnagar <rohan.bha09@gmail.com>
2022-07-05 10:52:22 -0400

    solved merge conflicts

--- src/app/dropdown.component.ts
+++ src/app/dropdown.component.ts
@@ -5,8 +5,8 @@ import { Component, Directive, OnInit, Input } from '@angular/core';
     selector: 'dropdown',
     template: `
         <div style="display:inline">
-            <nb-select  placeholder="Select" [(selected)]="selectedItem">
-                <nb-select-label>{{ selectedItem }}</nb-select-label>
+            <nb-select  placeholder="Select" [(selected)]="selectedString">
+                <nb-select-label>{{ selectedString }}</nb-select-label>
                     <nb-option *ngFor="let option of this.options; let ind = index" value={{option}}>{{option}}</nb-option>
             </nb-select>
         </div>
@@ -14,25 +14,16 @@ import { Component, Directive, OnInit, Input } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class DropdownComponent implements OnInit {
-<<<<<<< HEAD
     @Input() options!: string[];
     @Input() selectedItem!: any;
-=======
     @Input() props!: string[];
 
-    options: string[] = [];
-
-    selectedItem: string = "";
->>>>>>> 3abb5047166662c50fe58a565d0d863be3579507
+    selectedString: string = "";
 
     constructor() {
     }
     ngOnInit(): void {
-<<<<<<< HEAD
-        console.log(this.options);
-=======
         this.options = this.props.slice(1, this.props.length);
->>>>>>> 3abb5047166662c50fe58a565d0d863be3579507
     }
     title = 'jsonTalkSoft';
 }
