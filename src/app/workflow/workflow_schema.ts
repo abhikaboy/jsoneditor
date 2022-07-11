a15a095dbcb1ea67daced711b9e336c2b3e1875f
Rohan Bhatnagar <rohan.bha09@gmail.com>
2022-07-11 14:22:08 -0400

    added workflow files + created schema file

---
+++ src/app/workflow/workflow_schema.ts
@@ -0,0 +1,14 @@
+export const workflow_schema = {
+  "$schema": "http://json-schema.org/draft-07/schema",
+  "title": "WorkflowTemplate",
+  "description": "Defines a valid workflow object",
+  "type": "object",
+  "required": [
+    "hello",
+  ],
+  "properties": {
+    "hello": {
+      
+    }
+  }
+}
